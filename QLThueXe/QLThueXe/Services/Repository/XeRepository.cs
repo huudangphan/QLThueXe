88bf3046e5f13621fa78be9228e6b27c3dce210c
huudangphan <64909707+huudangphan@users.noreply.github.com>
2021-12-27 20:40:01 +0700

    dang

--- QLThueXe/QLThueXe/Services/Repository/XeRepository.cs
+++ QLThueXe/QLThueXe/Services/Repository/XeRepository.cs
@@ -11,11 +11,13 @@ namespace QLThueXe.Services.Repository
         
         public string InsertData(string bien_so,int id_hang,string ten_xe,double gia_thue,string ma_ks,bool status)
         {
-            string query = string.Format(" select *  from insert_xe('{0}',{1},'{2}',{3},'{4}',{5}) commit", bien_so, id_hang, ten_xe, gia_thue, ma_ks,status);
+            string query = string.Format("BEGIN TRANSACTION; SET TRANSACTION ISOLATION LEVEL SERIALIZABLE; select *  from insert_xe('{0}',{1},'{2}',{3},'{4}',{5}) ", bien_so, id_hang, ten_xe, gia_thue, ma_ks,status);
             try
             {
-                DataBaseServices.Instance.ExecuteQuery("begin");
-                return DataBaseServices.Instance.ExecuteQuery(query);
+                
+                var a= DataBaseServices.Instance.ExecuteQuery(query);
+                DataBaseServices.Instance.ExecuteQuery("commit");
+                return a;
             }
             catch (Exception ex)
             {
@@ -34,7 +36,7 @@ namespace QLThueXe.Services.Repository
             if (gia_thue==0)
                 gia_thue = -1;
 
-            string query = string.Format("BEGIN TRANSACTION; SET TRANSACTION ISOLATION LEVEL REPEATABLE READ; select * from search_xe('{0}',{1},'{2}',{3},'{4}',{5}) ", bien_so, id_hang, ten_xe, gia_thue, ma_ks, status);
+            string query = string.Format("BEGIN TRANSACTION; SET TRANSACTION ISOLATION LEVEL SERIALIZABLE; select * from search_xe('{0}',{1},'{2}',{3},'{4}',{5}) ", bien_so, id_hang, ten_xe, gia_thue, ma_ks, status);
 
 
             try
@@ -53,7 +55,7 @@ namespace QLThueXe.Services.Repository
         }
         public string UpdateXe(string bien_so,double gia_thue,bool status)
         {
-            string query = string.Format(" BEGIN TRANSACTION; SET TRANSACTION ISOLATION LEVEL REPEATABLE READ; select * from update_xe('{0}',{1},{2}) ", bien_so, gia_thue, status);
+            string query = string.Format(" BEGIN TRANSACTION; SET TRANSACTION ISOLATION LEVEL SERIALIZABLE; select * from update_xe('{0}',{1},{2}) ", bien_so, gia_thue, status);
             string query2 = "commit;";
 
             try
